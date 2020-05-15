d49533289b0c69830460232dff225e5bd977123f
Ryan Tung <53037140+13126789556@users.noreply.github.com>
2020-05-15 02:59:33 -0700

    light controller

---
+++ ReturnOfTheKing/Assets/Script/Gameplay/LightController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightController : MonoBehaviour
+{
+    Boss boss;
+    Renderer mesh;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mesh = GetComponent<Renderer>();
+        boss = GetComponentInParent<Boss>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mesh.enabled = boss.isAvailable ? true : false;
+    }
+}
