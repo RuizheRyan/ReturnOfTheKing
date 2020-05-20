e45ad25ccf1080423cbf843d8fbb79e7c7b1f230
Ryan Tung <53037140+13126789556@users.noreply.github.com>
2020-05-19 23:30:35 -0700

    add player camera

---
+++ ReturnOfTheKing/Assets/Script/Gameplay/CamraController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class CamraController : MonoBehaviourPun
+{
+    public bool isBossCam;
+    [SerializeField]
+    Transform player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (isBossCam && !PhotonNetwork.IsMasterClient)
+        {
+            gameObject.SetActive(false);
+        }
+        else
+        {
+            player = player == null ? transform.parent : player;
+            if (!player.GetComponent<PhotonView>().IsMine)
+            {
+                gameObject.SetActive(false);
+            }
+            transform.SetParent(null);
+        }
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = player.position;
+    }
+}
