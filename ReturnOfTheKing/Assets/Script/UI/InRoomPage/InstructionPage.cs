1aef0039123a34ac019e1b4881007c004117e539
haruorange <44792672+haruorange@users.noreply.github.com>
2020-05-21 11:37:14 -0700

    instruction
    instruction implemented

---
+++ ReturnOfTheKing/Assets/Script/UI/InRoomPage/InstructionPage.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstructionPage : MonoBehaviour
+{
+	[SerializeField] private GameObject hiderInfo;
+	[SerializeField] private GameObject seekerInfo;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+	public void ShowSeeker()
+	{
+		seekerInfo.SetActive(true);
+	}
+	public void HideSeeker()
+	{
+		seekerInfo.SetActive(false);
+	}
+
+	public void ShowHider()
+	{
+		hiderInfo.SetActive(true);
+	}
+	public void HideHider()
+	{
+		hiderInfo.SetActive(false);
+	}
+
+}
